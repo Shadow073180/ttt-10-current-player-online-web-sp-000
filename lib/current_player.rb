bf348a46fed6e9490b838be5e184b71e8b226d76
David masterson <david73180@gmail.com>
2018-09-30 01:00:39 +0000

    automatically backed up by learn

---
+++ lib/current_player.rb
@@ -0,0 +1,21 @@
+def turn_count(board)
+  counter = 0
+  board.each do |x|
+    if x == "X" || x == "O"
+      counter += 1
+      return counter
+    elsif x == " " || x == "" || x == nil
+      return counter
+    end
+  end
+end
+
+
+def current_player(board)
+  counter2 = turn_count(board)
+  if counter2 == 0 || counter2.odd?
+    return "X"
+  elsif counter2.even?
+    return "O"
+  end
+end
